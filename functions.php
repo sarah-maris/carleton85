cf60db6dd4aabb263ed4c5775aa043c6ea5ea94f
sarah_maris <sarah.87.maris@gmail.com>
2015-06-25 13:06:28 -0400

    Add basic files for slider

--- functions.php
+++ functions.php
@@ -1,8 +1,31 @@
 <?php
-//
-// Recommended way to include parent theme styles.
-//  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
-//  
+/**
+ * Additional Functions
+ *
+ * Designed by Sarah Maris for Carleton85.com based on TwentyFifteen theme
+ * revised 06-25-15
+ *
+ */
+
+/*
+ * Contents:
+ *
+ *  1.0 - Enqueue parent theme styles
+ *
+ *  2.0 - Add flex slider
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+/* 1.0 Enqueue parent theme styles 
+-------------------------------------------------*/
+ 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -11,6 +34,14 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
 }
-//
-// Your code goes below
-//
+
+/* 2.0 Add flex slider 
+------------------------------------------------ */
+
+/* 2.1  Create Slider Post Type */
+require( get_stylesheet_directory() . '/inc/slider/slider_post_type.php' );
+
+/* 2.2 Create Slider */
+require( get_stylesheet_directory() . '/inc/slider/slider.php' );
+
+?>
